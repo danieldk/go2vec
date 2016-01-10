b4ba57afa808896042799b10548d7abc38b229b5
Daniël de Kok <me@danieldk.eu>
2016-01-10 16:54:19 +0100

    Add missing documentation.

--- cmd/common/common.go
+++ cmd/common/common.go
@@ -2,6 +2,8 @@ package common
 
 import "log"
 
+// ExitIfError exits a program with a fatal error message, if
+// the supplied error is not nil.
 func ExitIfError(prefix string, err error) {
 	if err != nil {
 		log.Fatal(prefix, err.Error())
